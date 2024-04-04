fd84ddd0742ee3431d79ae81765dddc5ad0f4a6e
Nguyen Minh Anh <xiaosasori@gmail.com>
2024-04-04 10:24:37 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import WordleBoard from './components/WordleBoard.vue'
+
+</script>
+
+<template>
+  <WordleBoard word-of-the-day="You did it!" />
+</template>
---
+++ src/components/WordleBoard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { VICTORY_MESSAGE } from '@/settings'
+
+defineProps<{
+  wordOfTheDay: string
+}>()
+</script>
+
+<template>
+  <input type="text">
+  <p>You won!</p>
+  <h1>{{ VICTORY_MESSAGE }}</h1>
+</template>
